fe5fd2b55965c8f19879da0b1aa32ae723092224
U-RAHMEH\Rahmeh <rjf@bu.edu>
2015-12-08 10:41:25 -0500

    "heyo"

--- input_window.py
+++ input_window.py
@@ -4,6 +4,7 @@ Created on Fri Nov 27 22:03:40 2015
 
 @author: Rahmeh
 """
+MyInfo={}
 
 from tkinter import *
 def prompt_user():
@@ -83,14 +84,13 @@ def prompt_user():
         "Title":[ttl_1.get(),ttl_2.get(),ttl_3.get(),ttl_4.get(),ttl_5.get(),ttl_6.get()], 
         "section":[sect_1.get(),sect_2.get(),sect_3.get(),sect_4.get(),sect_5.get(),sect_6.get()],
         "other":[no_monday.get(), no_friday.get()]}
-        print (class1)
-        return class1
+        MyInfo['classes']=class1
+        root.destroy()
     
     Button(root,text='Enter', relief=GROOVE,width=15, height=2, command=collect_data).grid(column=6, sticky=SE)
-    
-       
-    root.mainloop()
-    return(collect_data())
 
-L = prompt_user()
-#print (L)
+    root.mainloop()    
+    
+
+prompt_user()
+print (MyInfo)
